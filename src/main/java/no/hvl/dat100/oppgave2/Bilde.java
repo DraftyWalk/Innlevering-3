f408d7b5b65dd65d973a62b716fc74b37369981d
DraftyWalk <letberger@outlook.com>
2025-11-11 11:41:15 +0100

    ferdig med to-do, untatt oppgave valfi oppg 6 del

--- src/main/java/no/hvl/dat100/oppgave2/Bilde.java
+++ src/main/java/no/hvl/dat100/oppgave2/Bilde.java
@@ -4,30 +4,30 @@ import no.hvl.dat100.common.TODO;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
+	private String url;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+        super(id,bruker,dato,0,tekst);
+        this.url = url;
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+        super(id,bruker,dato,likes,tekst);
+        this.url = url;
 	}
 	
 	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+		return this.url;
 
 	}
 
-	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+	public void setUrl(String url) {this.url = url;}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+        return "BIlDE\n" + getId() + "\n" + getBruker() + "\n" + getDato() + "\n" + getLikes() + "\n"+ getTekst() + "\n" + getUrl() + "\n";
 
-	}
+    }
 
 	// Metoden nedenfor er kun for valgfri oppgave 6
 	public String toHTML() {
